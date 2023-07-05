5b8c82ee55dfef53c09c6ba4d95ce40abbb7c144
Kuch Darith <kuchdarith@KuchdeMacBook-Pro.local>
2023-07-05 18:22:54 +0700

    add'

--- demo/src/main/java/com/morecambodia/demo/MainActivity.kt
+++ demo/src/main/java/com/morecambodia/demo/MainActivity.kt
@@ -2,10 +2,79 @@ package com.morecambodia.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.morecambodia.mcadmoblibrary.AdmobService
 
 class MainActivity : AppCompatActivity() {
+    private var tag = "Admob Log"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.banner).setOnClickListener {
+            loadBanner()
+        }
+        findViewById<Button>(R.id.intershow).setOnClickListener {
+            loadInterstitialAd()
+        }
+        findViewById<Button>(R.id.reward).setOnClickListener {
+            loadRewardedAd()
+        }
+    }
+    /**
+     * Load Admob banner
+     */
+    private fun loadBanner(){
+        AdmobService(this).setOnListener { status, adType, type, amount ->
+            if (adType == AdmobService.AdType.AdView) {
+                when (status) {
+                    AdmobService.AdStatus.LOADED -> {
+                        Log.i(tag, "AdView")
+                    }
+
+                    else -> {}
+                }
+            }
+        }.onStartAdBanner(viewBanner = findViewById(R.id.adBanner))
+    }
+    /**
+     * Load Admob InterstitialAd
+     */
+    private fun loadInterstitialAd(){
+        AdmobService(this).setOnListener { status, adType, type, amount->
+            if (adType == AdmobService.AdType.InterstitialAd) {
+                when (status) {
+                    AdmobService.AdStatus.LOADED -> {
+                        Log.i(tag, "InterstitialAd")
+                    }
+
+                    else -> {
+
+                    }
+                }
+            } else {
+
+            }
+        }.onStartAdInterstitial()
+    }
+    /**
+     * Load Admob RewardedAd
+     */
+    private fun loadRewardedAd(){
+        AdmobService(this).setOnListener { status, adType, type, amount ->
+            if (adType == AdmobService.AdType.RewardedAd) {
+                when (status) {
+                    AdmobService.AdStatus.REWARDED -> {
+                        Log.i(tag, "REWARDED")
+                    }
+
+                    else -> {
+
+                    }
+
+                }
+            } else {
+            }
+        }.onStartReward()
     }
 }
